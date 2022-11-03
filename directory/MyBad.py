543951732fcccad4bad565f0f1bde06f6ff6853b
Сорокин Алексей <jiysuan@mail.ru>
2022-11-03 12:02:07 +0300

    Не забываю страдать

--- directory/MyBad.py
+++ directory/MyBad.py
@@ -2,23 +2,27 @@ import telebot
 from telebot import TeleBot
 from modules.read_data import read_data
 from modules.new_data import new_data
-from modules.export_data import export_data
 from modules.import_data import import_data
 
 
 bot = TeleBot('5771267528:AAEpt6yUkxZcavAtrnYz7PLlgrM9eqCa6sI')
+
 
 @bot.message_handler(commands=['start'])
 def first_message(message):
     bot.send_message(message.chat.id,
                      text=(f'Это телефонный справочник "ДТП Акеруса"! Вот список команд: \n'
                            '\n'
+                           '/start - на случай если ты забыл какую команду только что использовал\n'
                            '/read - посмотреть справочник\n'
                            '/edit - внести новые данные в справочник\n'
                            '/export - получить справочник в виде файла\n'
-                           '/import - прислать файл, данные из которого попадут в справочник\n'
+                           'А ещё можно просто отправить боту txt файл с данными для справочника. '
+                           'Данные в файле должны быть в формате: ФИО, номер, комментарий. '
+                           'Обязательно разделять запятыми вышеупомянутые пункты содержимого файла. '
+                           'Информация из файла будет добавлена в конец справочника.\n'
                            '\n'
-                           'Выберите команду'))
+                           'Выберите команду или отправьте файл.'))
 
 
 @bot.message_handler(commands=['read'])
@@ -40,6 +44,7 @@ def bot_edit_data(message):
 def edit_one_line(message: telebot.types.Message):
     next_message = bot.send_message(chat_id=message.from_user.id, text='Укажите через запятую: ФИО, номер, комментарий:' )
     bot.register_next_step_handler(callback=new_data_one, message=next_message)
+    bot.send_message(chat_id=message.from_user.id, text='Готово')
 
 
 def new_data_one(message):
@@ -50,21 +55,21 @@ def new_data_one(message):
 def edit_three_lines(message):
     next_message = bot.send_message(chat_id=message.from_user.id, text='Укажите через запятую: ФИО, номер, комментарий:' )
     bot.register_next_step_handler(callback=new_data_three, message=next_message)
+    bot.send_message(chat_id=message.from_user.id, text='Готово')
 
 
 def new_data_three(message):
     new_data(0, message.text)
 
 
-@bot.message_handler(commands=['import'])
-def bot_import_data(msg):
-    file = bot.get_file(msg.document.file_id)
+@bot.message_handler(content_types=['document'])
+def bot_import_data(message: telebot.types.Message):
+    file = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file.file_path)
-    path = "export/" + msg.document.file_name
-    with open(path, "wb") as f_out:
+    with open('import_data.txt', "wb") as f_out:
         f_out.write(downloaded_file)
-    import_interface(path)
-    bot.send_message(chat_id=msg.from_user.id, text="Данные импортированы")
+    import_data()
+    bot.send_message(chat_id=message.from_user.id, text='Готово')
 
 
 @bot.message_handler(commands=['export'])
--- directory/modules/import_data.py
+++ directory/modules/import_data.py
@@ -1,6 +1,7 @@
 def import_data():
-    with open("../export_result.txt", "r", encoding='UTF-8') as file:
+    with open("import_data.txt", "r") as file:
         data = file.read()
-    export_file = open("database.txt", "w")
+    export_file = open("database.txt", "a")
     export_file.write(data)
     export_file.close()
+
